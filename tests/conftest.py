13b081360170bccb30b386f1ef3759450b7aa945
Uglykoyote <uglykoyote@yandex.ru>
2025-11-10 00:57:37 +0100

    Tests fixed

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+# tests/conftest.py
+import os
+import sys
+
+# Add project root to PYTHONPATH so `modules` becomes importable
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
